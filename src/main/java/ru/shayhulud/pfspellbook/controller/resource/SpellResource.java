f5eacae1a720e94722dc8225878e0810ccd5183b
Dmitry Skirdin <bskirdin@gmail.com>
2020-02-11 16:12:00 +0300

    #2 add swagger configuration

--- src/main/java/ru/shayhulud/pfspellbook/controller/resource/SpellResource.java
+++ src/main/java/ru/shayhulud/pfspellbook/controller/resource/SpellResource.java
@@ -1,10 +1,30 @@
 package ru.shayhulud.pfspellbook.controller.resource;
 
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.shayhulud.pfspellbook.domain.model.Spell;
+import ru.shayhulud.pfspellbook.domain.repository.SpellRepository;
 
 /**
  * Resource for managing spell library.
  */
+@Slf4j
 @RestController
+@RequiredArgsConstructor
+@Api(value = "/api/spell", tags = {"spell", "resource"})
 public class SpellResource {
+
+	private final SpellRepository spellRepository;
+
+	@PostMapping("/api/spell")
+	public Spell save(@RequestBody Spell spell) {
+		log.debug("REST request to save Spell : {}", spell);
+		//TODO: realize;
+		return spell;
+	}
+
 }
